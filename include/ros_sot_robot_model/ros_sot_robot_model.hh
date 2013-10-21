43fbbd7fc3a6f0787222fb5f2fb3fa4a588cc3d4
graiola <student@lorien.pal-local>
2013-10-21 12:29:10 +0200

    added a list of actuated joints ordered by rank and cp advices.

--- include/ros_sot_robot_model/ros_sot_robot_model.hh
+++ include/ros_sot_robot_model/ros_sot_robot_model.hh
@@ -1,24 +1,54 @@
-#ifndef ROS_SOT_ROBOT_MODEL_HH
+/*
+ * Software License Agreement (Modified BSD License)
+ *
+ *  Copyright (c) 2013, PAL Robotics, S.L.
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PAL Robotics, S.L. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \author Gennaro Raiola
+ *   inspired on the RosRobotModel class written by Thomas Moulard, available here http://wiki.ros.org/dynamic_graph_bridge#RosRobotModel.
+ */
+
+# ifndef ROS_SOT_ROBOT_MODEL_HH
 # define ROS_SOT_ROBOT_MODEL_HH
-# include <list>
 # include <string>
 
-//# include <jrl/mal/boost.hh>
 # include "jrl/mal/matrixabstractlayer.hh"
 
 namespace ml = maal::boost;
 
-# include <jrl/dynamics/dynamicsfactory.hh>
-
-//# include <sot/core/flags.hh>
 # include <dynamic-graph/command.h>
 # include <dynamic-graph/entity.h>
-//# include <dynamic-graph/pool.h>
-# include <dynamic-graph/signal-ptr.h>
-# include <dynamic-graph/signal-time-dependent.h>
-//# include <sot/core/exception-dynamic.hh>
-# include <sot/core/matrix-homogeneous.hh>
 # include <sot-dynamic/dynamic.h>
+# include <jrl/dynamics/urdf/parser.hh>
 # include "XmlRpcValue.h"
 
 namespace dynamicgraph
@@ -31,7 +61,7 @@ namespace dynamicgraph
   ///
   /// This relies on jrl_dynamics_urdf to load the model and jrl-dynamics
   /// to realize the computation.
-  class RosSotRobotModel : public sot::Dynamic
+  class RosSotRobotModel : public sot::Dynamic, jrl::dynamics::urdf::Parser
   {
     DYNAMIC_GRAPH_ENTITY_DECL();
   public:
@@ -41,8 +71,10 @@ namespace dynamicgraph
     virtual ~RosSotRobotModel();
 
     void loadUrdf(const std::string& filename);
-    void setNamespace (const std::string& ns);
     void loadFromParameterServer();
+    void publishJointNames();
+    void setNamespace (const std::string& ns);
+    void setParameterName (const std::string& pn);
     Vector curConf() const;
 
   protected:
@@ -57,13 +89,16 @@ namespace dynamicgraph
   private:
 
     /// \brief Name of the parameter where the joints list will be published
-    std::string jointsParameterName_;
+    std::string pn_;
 
-    /// \brief Name of the controller namespace
+    /// \brief Controller namespace
     std::string ns_;
+
+    /// \brief List of actuated joints ordered by rank
+    XmlRpc::XmlRpcValue jointNames_;
 
 
   };
 } // end of namespace dynamicgraph.
 
-#endif //! ROS_SOT_ROBOT_MODEL_HH
+# endif //! ROS_SOT_ROBOT_MODEL_HH
--- src/ros_sot_robot_model.cpp
+++ src/ros_sot_robot_model.cpp
@@ -1,23 +1,54 @@
-#include <limits>
+/*
+ * Software License Agreement (Modified BSD License)
+ *
+ *  Copyright (c) 2013, PAL Robotics, S.L.
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PAL Robotics, S.L. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include <boost/bind.hpp>
+/** \author Gennaro Raiola
+ *   inspired on the RosRobotModel class written by Thomas Moulard, available here http://wiki.ros.org/dynamic_graph_bridge#RosRobotModel.
+ */
 
-#include <ros/ros.h>
-
-#include <dynamic-graph/all-commands.h>
-#include <dynamic-graph/factory.h>
-#include <dynamic-graph/null-ptr.hh>
-#include <jrl/dynamics/urdf/parser.hh>
-
-#include "dynamic_graph_bridge/ros_init.hh"
-#include <ros_sot_robot_model/ros_sot_robot_model.hh>
+# include <ros/ros.h>
+# include <dynamic-graph/all-commands.h>
+# include <dynamic-graph/factory.h>
+# include "dynamic_graph_bridge/ros_init.hh"
+# include <ros_sot_robot_model/ros_sot_robot_model.hh>
 
 namespace dynamicgraph
 {
 
 RosSotRobotModel::RosSotRobotModel(const std::string& name)
     : Dynamic(name,false),
-      jointsParameterName_("jrl_joints_list"),
+      pn_("jrl_joints_list"),
       ns_("sot_controller")
 {
     std::string docstring;
@@ -38,9 +69,15 @@ RosSotRobotModel::RosSotRobotModel(const std::string& name)
 
     docstring =
             "\n"
-            "  Set the controller namespace."
+            "  Set the namespace."
             "\n";
     addCommand("setNamespace", command::makeCommandVoid1(*this,&RosSotRobotModel::setNamespace,docstring));
+
+    docstring =
+            "\n"
+            "  Set the parameter name."
+            "\n";
+    addCommand("setParameterName", command::makeCommandVoid1(*this,&RosSotRobotModel::setNamespace,docstring));
 
     docstring =
             "\n"
@@ -54,13 +91,10 @@ RosSotRobotModel::~RosSotRobotModel()
 
 void RosSotRobotModel::loadUrdf (const std::string& filename)
 {
-    jrl::dynamics::urdf::Parser parser;
 
-    m_HDR = parser.parse(filename);
+    m_HDR = parse(filename);
 
-    ros::NodeHandle nh(ns_);
-
-    nh.setParam(jointsParameterName_, parser.JointsNamesByRank_);
+    publishJointNames();
 }
 
 void RosSotRobotModel::setNamespace (const std::string& ns)
@@ -68,10 +102,13 @@ void RosSotRobotModel::setNamespace (const std::string& ns)
     ns_ = ns;
 }
 
+void RosSotRobotModel::setParameterName (const std::string& pn)
+{
+    pn_ = pn;
+}
+
 void RosSotRobotModel::loadFromParameterServer()
 {
-    jrl::dynamics::urdf::Parser parser;
-
     rosInit (false);
     std::string robotDescription;
     ros::param::param<std::string> (ns_ + "/robot_description", robotDescription, "");
@@ -79,11 +116,23 @@ void RosSotRobotModel::loadFromParameterServer()
     if (robotDescription.empty ())
         throw std::runtime_error("No model available as ROS parameter. Fail.");
 
-    m_HDR = parser.parseStream (robotDescription);
+    m_HDR = parseStream(robotDescription);
+
+    publishJointNames();
+
+}
+
+void RosSotRobotModel::publishJointNames(){
+
+    // Load a list of joints ordered by rank
+    std::vector<CjrlJoint*> tmp_jv = m_HDR->jointVector();
+    std::vector<CjrlJoint*> actJointsVect = actuatedJoints();
+    for (int i=0;i<tmp_jv.size();i++)
+        if (std::find(actJointsVect.begin(), actJointsVect.end(),tmp_jv[i])!=actJointsVect.end())
+            JointsNamesByRank_[tmp_jv[i]->rankInConfiguration()-6] = tmp_jv[i]->getName();
 
     ros::NodeHandle nh(ns_);
-
-    nh.setParam(jointsParameterName_, parser.JointsNamesByRank_);
+    nh.setParam(pn_, jointNames_);
 
 }
 
